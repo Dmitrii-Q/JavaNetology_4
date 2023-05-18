55c8d6b621e77822c22b8bfccf852218c6ce1ddc
Dmitrii-Q <120948230+Dmitrii-Q@users.noreply.github.com>
2023-05-18 15:24:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
 }
+
+500
+
+Process finished with exit code 0
